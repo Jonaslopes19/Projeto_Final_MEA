82164c6dfa9f6c40da65c44776df50065bcbc18b
Jonas da Silva Lopes <jonas.slopes@hotmail.com>
2021-11-25 20:51:39 -0300

    Adicionado a pinagem e início das telas

--- CCM.cpp
+++ CCM.cpp
@@ -1,103 +1,84 @@
 //Projeto Final de Máquina Elétricas - Grupo 4
 //CCM para controle de um motor brushless e um motor CC
-
 #include "mbed.h"
 
-InterruptIn User (USER_BUTTON);
+Serial pc (USBTX, USBRX);
 
-PwmOut IN_1 (PA_8); // pinos para PWM
-PwmOut IN_2 (PA_9);
-PwmOut IN_3 (PA_10);
-DigitalOut EN_1 (PC_10); // pinos ENABLE
-DigitalOut EN_2 (PC_11);
-DigitalOut EN_3 (PC_12);
+//Declaração PWM Ponte-H motor CC
+PwmOut mos1 (D12);
+PwmOut mos2 (D11);
+PwmOut mos3 (D9);
+PwmOut mos4 (D8);
+//Declaração PWM controle motor de passo
+PwmOut mos5 (D7);
+PwmOut mos6 (D6);
+PwmOut mos7 (D5);
+PwmOut mos8 (D4);
 
-AnalogIn pot(PB_1);
+//Variáveis de controle da IHM
+bool m_cc = 0;
+bool m_passo = 0;
 
-double tempo;
-int controle;
-float pwm = 0.8;
-bool i = 0;
+bool enable = 0;
 
-void toggle(void);// Protótipo de função
+int ação = 0;
 
 
-int main()
-{
-    // configurações iniciais
-    IN_1.period_ms(1); 
-    IN_2.period_ms(1); 
-    IN_3.period_ms(1); 
-    EN_1 = 2; // Braço A
-    EN_2 = 2; // Braço B
-    EN_3 = 2; // Braço C
-    
-    User.rise(&toggle);
-    
-    while(1) 
-    {
-        tempo = 10-(8*(1-pot.read()));
-        
-        if (i == 1)
-        {
-            //Estado 1
-            EN_1 = 2;
-            EN_2 = 0;
-            EN_3 = 1;
-            IN_1.write(0);
-            IN_2.write(pwm);
-            IN_3.write(pwm);
-            wait_ms(tempo);   
-            
-            //Estado 2
-            EN_1 = 1;
-            EN_2 = 0;
-            EN_3 = 2;
-            IN_1.write(0);
-            IN_2.write(0);
-            IN_3.write(pwm);
-            wait_ms(tempo);
-            
-            //Estado 3
-            EN_1 = 1;
-            EN_2 = 2;
-            EN_3 = 0;
-            IN_1.write(pwm);
-            IN_2.write(0);
-            IN_3.write(pwm);
-            wait_ms(tempo);
-            
-            //Estado 4
-            EN_1 = 2; 
-            EN_2 = 1; 
-            EN_3 = 0; 
-            IN_1.write(pwm); 
-            IN_2.write(0); 
-            IN_3.write(0); 
-            wait_ms(tempo);
-            
-            //Estado 5
-            EN_1 = 0; 
-            EN_2 = 1; 
-            EN_3 = 2; 
-            IN_1.write(pwm); 
-            IN_2.write(pwm);
-            IN_3.write(0);
-            wait_ms(tempo);
-            
-            //Estado 6
-            EN_1 = 0; 
-            EN_2 = 2; 
-            EN_3 = 1; 
-            IN_1.write(0); 
-            IN_2.write(pwm); 
-            IN_3.write(0);
-            wait_ms(tempo);
-        }
-    }
-}
+
 
 void toggle()//Função para incrementar dentes quando houver uma borda de subida
         {
             i =! i;
         }
+
+//Declaração de portas e variáveis motor CC
+
+
+int main()
+{
+    printf(\r"Bem vindo professor");
+    wait(1.2);
+    printf(\r"ao centro de comando");
+    wait(1.2);
+    printf(\r"do grupo 4");
+    wait(1.2);
+    printf(\r"Jonas Luis Leonardo (no Balkins)");
+    wait(1.5);
+
+    while(1) 
+    {
+        switch (pc.getc())
+        {
+            case 'a': m_cc = 1; break;
+            case 'b': m_passo = 1; break; 
+            default: break;
+        }
+
+        // Controle do motor CC
+        if (m_cc == 1):
+        {
+            if (enable == 1)
+            {
+                if (ação == 0)
+                {
+
+                }
+
+                else if (ação == 1 )
+                {
+
+                }
+
+                else if (ação == 1 )
+                {
+                    
+                }
+            }
+        }
+
+        else if (m_passo == 1)
+        {
+
+        }
+    }
+}
